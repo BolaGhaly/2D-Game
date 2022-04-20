aca91640516c31415c0906ae5126905edfbb2de0
ReeseGanaway <93220118+ReeseGanaway@users.noreply.github.com>
2022-04-19 23:19:11 -0400

    Added game states (pause and play), game can now be paused. Items were and all other user interface features were disabled.

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -15,33 +15,33 @@ public class AssetSetter {
 
 	public void setObject() {
 
-		// ------------------ Keys -------------------
-		gp.objects[0] = new KeyObject(gp);
-		gp.objects[0].worldX = 22 * gp.tileSize;
-		gp.objects[0].worldY = 18 * gp.tileSize;
-
-		gp.objects[1] = new KeyObject(gp);
-		gp.objects[1].worldX = 24 * gp.tileSize;
-		gp.objects[1].worldY = 18 * gp.tileSize;
-
-		// ------------------ Doors ------------------
-		gp.objects[2] = new DoorObject(gp);
-		gp.objects[2].worldX = 26 * gp.tileSize;
-		gp.objects[2].worldY = 18 * gp.tileSize;
-
-		gp.objects[3] = new DoorObject(gp);
-		gp.objects[3].worldX = 28 * gp.tileSize;
-		gp.objects[3].worldY = 18 * gp.tileSize;
-
-		// ------------------ Chests -----------------
-		gp.objects[4] = new ChestObject(gp);
-		gp.objects[4].worldX = 30 * gp.tileSize;
-		gp.objects[4].worldY = 18 * gp.tileSize;
-
-		// ------------------ Boots ------------------
-		gp.objects[5] = new BootsObject(gp);
-		gp.objects[5].worldX = 34 * gp.tileSize;
-		gp.objects[5].worldY = 18 * gp.tileSize;
-
+//		// ------------------ Keys -------------------
+//		gp.objects[0] = new KeyObject(gp);
+//		gp.objects[0].worldX = 22 * gp.tileSize;
+//		gp.objects[0].worldY = 18 * gp.tileSize;
+//
+//		gp.objects[1] = new KeyObject(gp);
+//		gp.objects[1].worldX = 24 * gp.tileSize;
+//		gp.objects[1].worldY = 18 * gp.tileSize;
+//
+//		// ------------------ Doors ------------------
+//		gp.objects[2] = new DoorObject(gp);
+//		gp.objects[2].worldX = 26 * gp.tileSize;
+//		gp.objects[2].worldY = 18 * gp.tileSize;
+//
+//		gp.objects[3] = new DoorObject(gp);
+//		gp.objects[3].worldX = 28 * gp.tileSize;
+//		gp.objects[3].worldY = 18 * gp.tileSize;
+//
+//		// ------------------ Chests -----------------
+//		gp.objects[4] = new ChestObject(gp);
+//		gp.objects[4].worldX = 30 * gp.tileSize;
+//		gp.objects[4].worldY = 18 * gp.tileSize;
+//
+//		// ------------------ Boots ------------------
+//		gp.objects[5] = new BootsObject(gp);
+//		gp.objects[5].worldX = 34 * gp.tileSize;
+//		gp.objects[5].worldY = 18 * gp.tileSize;
+//		
 	}
 }
